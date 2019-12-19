23eb32544164d1b536223d5f05ee0147a39b5b7f
jackfreeloader74 <33938568+jackfreeloader74@users.noreply.github.com>
2019-12-19 17:05:24 +0000

    Add files via upload
    JS logic for portfolio summary screen redirect

--- js/Portfolio.js
+++ js/Portfolio.js
@@ -5,7 +5,7 @@ $(function() {
 		
         $.ajax({
             url: '/generatePortfolio',
-            data: "",
+            data: $('form').serialize(),
             type: 'POST',
             success: function(response) {
                 console.log(response);
