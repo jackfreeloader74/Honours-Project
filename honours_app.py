0fac61a1927eb2c530e8ac787aa2154c276ab84e
jackfreeloader74 <33938568+jackfreeloader74@users.noreply.github.com>
2019-12-19 17:02:32 +0000

    Add files via upload
    Added the redirect logic for when the portfolio generate button is clicked

--- honours_app.py
+++ honours_app.py
@@ -8,12 +8,26 @@ def main():
     print("Hello world")
     return render_template('index.html')
 
+@app.route('/ShowPortfolio')
+def ShowPortfolio():
+    print("Showing")
+    return render_template('portfolio_summary.html')
+
+
 @app.route('/generatePortfolio', methods=['POST'])
 def generatePortfolio():
-    print("Hello")
-    Sharpe = hp.OptimizePortfolio()
-    print("The sharp ratio is ", Sharpe)
-    return "HI"
+    _ticker1 = request.form['inputTicker1']
+    _ticker2 = request.form['inputTicker2']
+    _ticker3 = request.form['inputTicker3']
+    
+    print("Tickers ", _ticker1, _ticker2, _ticker3)
+    #Sharpe = hp.OptimizePortfolio()
+    #print("The sharp ratio is ", Sharpe)
+    print("Render Template")
+  
+    return redirect('/ShowPortfolio')
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
