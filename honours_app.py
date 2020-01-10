aa83ba1b3e422598488a02952f968128edcda458
jackfreeloader74 <33938568+jackfreeloader74@users.noreply.github.com>
2020-01-10 14:20:55 +0000

    10/01/2020
    Renamed optimization file to opt_aglorithms.py
    
    Server is now able to return the portfolios expected return to the client, along with all details in the table (Weights and tickers)

--- honours_app.py
+++ honours_app.py
@@ -1,24 +1,39 @@
 from flask import Flask, render_template, redirect, request, json, send_file, current_app as app, send_from_directory
-import HonoursProjectStart as hp
+from flask import url_for
+import opt_algorithms as hp
 import flask
 import pdfkit
+import ast
 
 
 app = Flask(__name__, static_folder='static')
 config = pdfkit.configuration(wkhtmltopdf='C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe')
 
-Return = 0
 
 @app.route("/")
 def main():
-    print("Hello world")
     return render_template('index.html')
 
 @app.route('/ShowPortfolio')
 def ShowPortfolio():
     print("Showing")
+    Return = request.args['Return']
+    Weights = request.args['weights']
+    Tickers = request.args['tickers']
+
+    # url_for converts the array to a string, literal_eval converts it back to an array
+    Weights = ast.literal_eval(Weights)
+    Weights = [ weight * 100 for weight in Weights ]
+    # Round weights to 2 dec places, maybe should do this in OPT code?
+    Weights = [ round(weight,2) for weight in Weights ]
+
+    Tickers = ast.literal_eval(Tickers)
+    
+
+    ## REMEMBER CURRENTLY USING STATIC apple ticker
     return render_template('portfolio_summary.html', name = 'Portfolio Weights', expected = Return, 
-                           url ='static/images/pie_chart.png')
+                           w1=Weights[0],w2=Weights[1],w3=Weights[2],w4=Weights[3],
+                           t1="AAPL", t2=Tickers[0], t3=Tickers[1], t4=Tickers[2], url ='static/images/pie_chart.png')
     
 @app.route('/generatePortfolio', methods=['POST'])
 def generatePortfolio():
@@ -32,11 +47,16 @@ def generatePortfolio():
     _ticker1 = request.form['inputTicker1']
     _ticker2 = request.form['inputTicker2']
     _ticker3 = request.form['inputTicker3']
+
+    tickers = [_ticker1,_ticker2, _ticker3 ]
+    print("Tick", tickers)
+
     
-   
-    Return = hp.OptimizePortfolio(_ticker1, _ticker2, _ticker3, expected_return )
-    
-    return redirect('/ShowPortfolio')
+    Return, weights = hp.OptimizePortfolio(_ticker1, _ticker2, _ticker3, expected_return )
+  
+    weights = list(weights)
+    return redirect(url_for('.ShowPortfolio', Return=Return, weights=str(weights), tickers=str(tickers) ) )
+  
   
 
 @app.route('/generatePDF', methods=['POST'])
