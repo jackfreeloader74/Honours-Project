0d63b2c4fdd37c83ab2be8d9dd315ef9a06540b0
jackfreeloader74 <33938568+jackfreeloader74@users.noreply.github.com>
2020-01-16 13:38:27 +0000

    Added weights and tickers to ajax request
    Ajax request for the generatePDF service

--- js/Portfolio.js
+++ js/Portfolio.js
@@ -1,9 +1,8 @@
 $(function() {
     $('#generatePortfolioBtn').click(function() {
 		
-		
-		
-        $.ajax({
+	
+        /*$.ajax({
             url: '/generatePortfolio',
             data: $('form').serialize(),
             type: 'POST',
@@ -13,20 +12,19 @@ $(function() {
             error: function(error) {
                 console.log(error);
             }
-        });
+        }); */
     });
 });
 
 
 
 $(function() {
-    $('#exportToPdfBtn').click(function() 		
+    $('#generatePortfolioBtn').click(function() {
 		
-		alert("Hello");
-		
-        $.ajax({
-            url: '/generatePDF',
-            data: "stuff",
+	
+        /*$.ajax({
+            url: '/generatePortfolio',
+            data: $('form').serialize(),
             type: 'POST',
             success: function(response) {
                 console.log(response);
@@ -34,6 +32,61 @@ $(function() {
             error: function(error) {
                 console.log(error);
             }
-        });
+        }); */
+    });
+});
+
+
+
+
+
+$(function() {
+    $('#exportToPdfBtn').click(function() {	
+		
+		var exp_return = $("p").eq(1).text()
+		
+		
+		// Obtain portfolio tickers		
+		var t1= $("#t1").text()
+		var t2 = $("#t2").text()
+		var t3 = $("#t3").text()
+		var t4 = $("#t4").text()
+		
+		tickers = [t1, t2, t3, t4]
+		tickers = JSON.stringify(tickers);
+		
+		
+		// Obtain corresponding weights
+		var w1 = $("#w1").text()
+		var w2 = $("#w2").text()
+		var w3 = $("#w3").text()
+		var w4 = $("#w4").text()
+		
+		weights = [w1, w2, w3, w4]	
+		weights = JSON.stringify(weights);
+		
+		
+		
+		/* Add them as parameters to request */
+		var data = {
+			Return: exp_return,
+			Tickers: tickers,
+			Weights: weights
+		}
+		
+		
+		
+        $.ajax({
+            url: '/generatePDF',
+            data: data,
+            type: 'GET',
+            success: function(response) {
+                window.location.href = '/ShowPDF'
+				console.log(response);
+            },
+            error: function(error) {
+               
+            }
+        });  
     });
 });
